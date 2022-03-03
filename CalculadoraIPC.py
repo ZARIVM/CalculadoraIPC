c288b5e99593a6704ae58208a76a9b355eb84b01
ZARIVM <zarivanmarin@gmail.com>
2022-03-03 17:21:03 -0500

    ingreso datos usuario

--- CalculadoraIPC.py
+++ CalculadoraIPC.py
@@ -1,11 +1,22 @@
 import pandas as pd
 
-df = pd.read_excel('C://Users//ZAR//Drive//CalculadoraIPC//CalculadoraDeflactorIPC.xlsx', sheet_name='Indice', skiprows=1,)#.pivot(columns = 0,  values = 1)
+#df = pd.read_excel('C://Users//ZAR//Drive//CalculadoraIPC//CalculadoraDeflactorIPC.xlsx', sheet_name='Indice', skiprows=1,)#.pivot(columns = 0,  values = 1)
 #Lista = {col: df[col].dropna().tolist() for col in df.columns}
-file2 = 20/20/20
-
+df = pd.read_excel('CalculadoraDeflactorIPC.xlsx')
 print(df)
 #print()
 #df se file1
 #file1 = file1.merge(file2[['columns', 'you', 'want', 'to','return']], how=’left’, on=’common-column-to-look-for’)
-df = df.merge(file2[['columns', 'you', 'want', 'to','return']], how=’left’, on=’common-column-to-look-for’)
+#df = df.merge(file2[['columns', 'you', 'want', 'to','return']], how=’left’, on=’common-column-to-look-for’)
+Fecha_inicio = input('Introduzca la fecha a consultar')
+Fecha_termino = input('Introduzca la fecha término a consultar')
+Valor_indicador = int(input('Introduzca el valor a calcular'))
+
+Valor_ajustado = df[['Valor calculado']==]
+
+
+
+#https://www.youtube.com/watch?v=Oby3ksXd1oY&ab_channel=cctmexico
+
+
+
