0176c614a817d0a9733eeabe256d5dfb8e39c69f
ZARIVM <zarivanmarin@gmail.com>
2022-03-08 13:54:19 -0500

    Cambio ecuación de cálculo

--- CalculadoraIPC.py
+++ CalculadoraIPC.py
@@ -1,20 +1,43 @@
+from numpy import float16
 import pandas as pd
 
 #df = pd.read_excel('C://Users//ZAR//Drive//CalculadoraIPC//CalculadoraDeflactorIPC.xlsx', sheet_name='Indice', skiprows=1,)#.pivot(columns = 0,  values = 1)
 #Lista = {col: df[col].dropna().tolist() for col in df.columns}
-df = pd.read_excel('CalculadoraDeflactorIPC.xlsx')
-print(df)
+
+
+calcula_df = pd.read_excel('CalculadoraDeflactorIPC.xlsx')
+print(calcula_df)
+
 #print()
 #df se file1
 #file1 = file1.merge(file2[['columns', 'you', 'want', 'to','return']], how=’left’, on=’common-column-to-look-for’)
 #df = df.merge(file2[['columns', 'you', 'want', 'to','return']], how=’left’, on=’common-column-to-look-for’)
+
+#def calcula_df ('Anio_Mes', 'IndiceIPC')
+   # Valor_resultado=
 Fecha_inicio = input('Introduzca la fecha a consultar')
 Fecha_termino = input('Introduzca la fecha término a consultar')
-Valor_indicador = int(input('Introduzca el valor a calcular'))
+Valor_indicador = input('Introduzca el valor a calcular')
 
-Valor_ajustado = df[['Valor calculado']==]
+filaIni_df = calcula_df[calcula_df['Anio_Mes']==Fecha_inicio]
+indicadorIni = float(filaIni_df['IndiceIPC'])
+
+filaFin_df = calcula_df[calcula_df['Anio_Mes']==Fecha_termino]
+indicadorFin = float(filaFin_df['IndiceIPC'])
+
+def calcula(indicadorIni, indicadorFin, Valor_indicador):
+    print("inicio", indicadorIni)
+    print("fin", indicadorFin)
+    print("Indica", Valor_indicador)
+    indicadorIPC = float(Valor_indicador)*(indicadorFin/indicadorIni)*100
+    
+    print(indicadorIPC)
+    return(indicadorIPC)
+
+calcula(indicadorIni, indicadorFin, Valor_indicador)
 
 
+#Valor_ajustado = df[['Valor calculado']==]
 
 #https://www.youtube.com/watch?v=Oby3ksXd1oY&ab_channel=cctmexico
 
